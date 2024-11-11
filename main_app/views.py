5963d2405db6e6e75f6f09747310b6b8cfd21b3c
Ndungu-Jn <ndungulink@gmail.com>
2024-11-11 13:48:17 +0300

    Templating.

--- main_app/views.py
+++ main_app/views.py
@@ -9,5 +9,19 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
+
 def services(request):
-    return render(request, 'services.html')
+    our_services = ["Bush camps", "Balloon Tours", "Hunting", "Village Visits"]
+    prices = 10000
+    date = '15-11-2024'
+    return render(request, 'services.html', {'services': our_services, 'prices': prices, 'date': date})
+
+# display data in our pages
+# loops
+# if statement ts
+# templating engine language
+
+#filters
+#images
+#static files
+
